6ce9c4f85428e28a4e42ff9c2821e24fd8056e52
Saeed Asgari <oclick021@gmail.com>
2020-12-22 21:42:02 +0100

    Algorithm changed

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ChemisteryMystery
         public double X { get; set; }
         public double X2 => 2 / X;
         public double X5 => (2 / X) * 3.76;
-        public double X6 => (9 / X) - 4;
+        public double X6 => (4 / X) - 4;
         static void Main(string[] args)
         {
             new Program();
@@ -17,7 +17,7 @@ namespace ChemisteryMystery
             bool isValid = false;
             while (!isValid)
             {
-                Console.WriteLine("Welcome to my Chemistery Mystery");
+                Console.WriteLine("Welcome to my CombustionProject ");
 
 
                 Console.WriteLine("What is stable Cp/Cv");
@@ -51,42 +51,40 @@ namespace ChemisteryMystery
                 }
 
 
-                const double x1 = 1;
-                const double x3 = 1;
-                const double x4 = 2;
+                const double X1 = 1;
+                const double X3 = 1;
+                const double X4 = 2;
                 const double tin = 298;
                 const double R = 8.315;
-                const double hMinCh4 = -74831;
-                const double hMinCO2 = -393546;
-                const double hMinH2o = -241845;
-                const double hMinN2 = 0;
-                const double hMinO2 = 0;
-                const double cMinCo2 = 56.21;
-                const double cMinH20 = 43.87;
-                const double cMinN2 = 33.71;
-                const double cMinO2 = 35.593;
+                const double h_Ch4 = -74831;
+                const double h_CO2 = -393546;
+                const double h_H2o = -241845;
+                const double h_N2 = 0;
+                const double h_O2 = 0;
+                const double c_Co2 = 56.21;
+                const double c_H20 = 43.87;
+                const double c_N2 = 33.71;
+                const double c_O2 = 35.593;
 
                 if (cType.ToLower() == "cp")
                 {
-                    var a = x1 * X * (hMinCh4) + X2 * X * hMinO2 + X2 * 3.76 * hMinN2;
-                    var b = x3 * hMinCO2 + x4 * hMinH2o + X5 * hMinN2 + X6 * hMinO2;
-                    var c = x3 * cMinO2 * tin + x4 * cMinH20 * tin + X5 * cMinN2 * tin + (X6 * hMinO2) * tin;
-                    var d = x3 * cMinCo2 + x4 * cMinH20 + X5 * cMinN2 + X6 * hMinO2;
+                    var a = X1 * h_Ch4 + X2 * h_O2 + X2 * 3.76 * h_N2;
+                    var b = X3 * h_CO2 + X4 * h_H2o + X5 * h_N2 + X6 * h_O2;
+                    var c = X3 * c_O2 * tin + X4 * c_H20 * tin + X5 * c_N2 * tin + X6 * c_O2 * tin;
+                    var d = X3 * c_Co2 + X4 * c_H20 + X5 * c_N2 + X6 * c_O2;
                     var tad = (a - b + c) / d;
                     Console.WriteLine($"The Tad based on CP is = {tad.ToString("0.###")}");
                 }
                 else
                 {
-                    var e = x1 * hMinCh4 + X2 * hMinO2 + X2 * 3.76 * hMinN2;
-                    var f = x3 * hMinCO2 + x4 * hMinH2o + X5 * hMinN2 + X6 * hMinO2;
-                    var g = R * tin * (x1 + X2);
-                    var h = (x3 + x4 + X5 + X6)*R;
-                    var cvTad = (e + f + g) / h;
-                    Console.WriteLine($"e = {e}");
-                    Console.WriteLine($"f = {f}");
-                    Console.WriteLine($"g = {g}");
-                    Console.WriteLine($"h = {h}");
-                    Console.WriteLine($"The Tad based on CV is {cvTad.ToString("0.###")}");
+                    var e = X1 * h_Ch4 + X2 * h_O2 + X2 * 3.76 * h_N2;
+                    var f = X3 * h_CO2 + X4 * h_H2o + X5 * h_N2 + X6 * h_O2;
+                    var g = X3 * c_O2 * tin + X4 * c_H20 * tin + X5 * c_N2 * tin + X6 * c_O2 * tin;
+                    var h = R * tin * X1 + R * tin * X2;
+                    var i = X3 * c_Co2 + X4 * c_H20 + X5 * c_N2 + X6 * c_O2;
+                    var j = X3 * R + X4 * R + X5 * R + X6 * R;
+                    var cvTad = (e - f + g - h) / (i - j);
+                    Console.WriteLine($"The Tad based on CV is={cvTad.ToString("0.###")}");
                 }
 
             }
